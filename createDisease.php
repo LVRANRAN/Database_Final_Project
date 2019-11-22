fb0f14295f7dd49748a3ddf19c04c8c43acd2de5
LVRANRAN <m13871193221@163.com>
2019-11-22 16:26:09 -0500

    1

---
+++ createDisease.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+
+//if(isset($_SESSION['admin'])) {
+//    header("Location: homepage.php");
+//}
+
+include_once 'connection.php';
+date_default_timezone_set("America/New_York");
+//set validation error flag as false
+$error = false;
+
+
+//check if form is submitted
+if (isset($_POST['create_disease'])) {
+    $disease_name = mysqli_real_escape_string($dbc, $_POST['disease_name']);
+
+    $tmp = mysqli_query($dbc, "select max(deid) as deid from disease");
+    $row = $tmp->fetch_assoc();
+    $deid = $row['deid'] + 1;
+    // $tdate = $_GET["tdate"];
+    // $pid = $_GET["pid"];
+
+//    $uid = $_GET["userid"];
+    //   $uid = 'lulu@gmail.com';
+    //  $create_time = date("Y-m-d H:i:s");
+    //  $status = 'fund_processing';
+    //if (!$error) {
+    //    if(mysqli_query($dbc, "INSERT INTO patient_treatment VALUES($treatment_date,$treatment_frequency,$treatment_status,$physician_id,$patient_id,$treatment_id)"))
+    //    {
+    //        $successmsg = "Successfully Registered! <a href='project.php?projectID=$pid'>Click here to View Project</a>";
+    //    } else {
+    //        $errormsg = "Error in registering...Please try again later!";
+    //    }
+    // }
+    if(!$error) {
+        $sql ="INSERT INTO disease VALUES($deid,'".$disease_name."')";
+        mysqli_query($dbc, $sql);
+        if($dbc->query($sql) == TRUE){
+            $successmsg =  "New record inserted successfully!";
+        } else {
+            $errormsg =  "Error in creating record, please check what you have entered!";
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Record</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" >
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+</head>
+
+<body>
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+        <!-- add header -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="homepage.php">Crowdfunding</a>
+        </div>
+        <!-- menu items -->
+        <!--<div class="collapse navbar-collapse" id="navbar1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="login.php">Login</a></li>
+                <li class="active"><a href="register.php">Sign Up</a></li>
+            </ul>
+        </div>-->
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 col-no-padding well">
+            <form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="createrecordform">
+                <fieldset>
+                    <legend>Create Record</legend>
+                    <div class="form-group">
+                        <label for="dename">Disease Name</label>
+                        <input type="text" name="disease_name" placeholder="Full Name" required value="<?php if($error) echo $disease_name; ?>" class="form-control" />
+                        <span class="text-danger"><?php if (isset($patient_name_error)) echo $patient_name_error; ?></span>
+                    </div
+                    <div class="form-group">
+                        <input type="submit" name="create_disease" value="Create Disease" class="btn btn-primary" />
+                    </div>
+                </fieldset>
+            </form>
+            <span class="text-success"><?php if (isset($successmsg)) { echo $successmsg; } ?></span>
+            <span class="text-danger"><?php if (isset($errormsg)) { echo $errormsg; } ?></span>
+        </div>
+    </div>
+    <!--<div class="row">
+        <div class="col-md-4 col-md-offset-4 text-center">
+        Already Registered? <a href="login.php">Login Here</a>
+        </div>
+    </div>-->
+</div>
+<script src="https://libs.baidu.com/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
